65e691014b537afa78bb62be1bfd8eaca4cb21c1
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-07-26 22:07:21 -0400

    adds delete

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -21,16 +21,15 @@ transactions.post("/", (req, res) => {
     res.json(transactionsData[transactionsData.length - 1]);
 });
 
-// transactions.delete("/:index", (req, res) => {
-//     const { index } = req.params;
-//     // const deletedTransaction = transactionsData.splice(index, 1);
-//     res.status(200).json(transactionsData.splice(index, 1));
-// });
-
 transactions.put("/:index", (req, res) => {
     const { index } = req.params;
     transactionsData[index] = req.body;
     res.status(200).json(transactionsData[index])
+});
+
+transactions.delete("/:index", (req, res) => {
+    const { index } = req.params;
+    res.status(200).json(transactionsData.splice(index, 1));
 });
 
 module.exports = transactions;
